430e3802c356edd10755a8d4dbcea01a035259b3
garbina98 <giadaandrea.garbin@student.h-farm.com>
2020-01-09 00:32:04 +0100

    Add docstrings where missing

--- birthdays_package/pyscripts/data_reader.py
+++ birthdays_package/pyscripts/data_reader.py
@@ -1,6 +1,13 @@
 import csv
 
 def parse_allowed_people(datafile):
+    """Read people names and birthdays from csv file
+
+    :param datafile: the path to the csv file
+    :type datafile: string
+    :return: The dictionary containing names and birthdays
+    :rtype: Dictionary Object
+    """
     with open(datafile) as people_data:
         people = {}
         csv_reader = csv.reader(people_data, delimiter=',')
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from birthdays_package.pyscripts import data_reader
 people_datafile = 'birthdays_package/data/people_infos.csv'
 
 def parse_arguments(people):
+    """Parse the arguments passed by the user
+
+    :param people: list of allowed names the user can get infos of
+    :type people: list
+    :return: arguments list
+    :rtype: list
+    """
     parser = argparse.ArgumentParser(
             description="Get name of someone you want to know the birthday of",
             prog="birthdays")
