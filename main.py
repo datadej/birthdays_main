93a41021d51cc4b836c9269fb5e80014ee709a57
GianlucaBasso <gb.basso14@gmail.com>
2020-01-08 21:00:35 +0100

    Use sys module and sys.argv array

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from birthdays_package import birthdays
+import sys
 
-birthdays.return_birthday('Albert Einstein')
-birthdays.return_birthday('Alan Turing')
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    birthday=birthdays.return_birthday(name)
+    if not birthday:
+        print("Sorry, we don't have {}'s birthday".format(name))
+    else:
+        print("{}' birthday is: {}".format(name, birthday))
+
+else:
+    print("Please tell me the person you want to know the birthday of.")
+    exit()
