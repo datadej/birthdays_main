ca368ee1daba7f03ee291ceda03d0ee4844ed238
Dostodejski <dejvid.de@gmail.com>
2020-01-09 01:50:50 +0100

    Add parsing function

--- dbmanager.py
+++ dbmanager.py
@@ -1,6 +1,7 @@
 import sqlite3
 import hashlib
 import random
+import argparse
 
 conn = None
 cursor = None
@@ -47,3 +48,18 @@ def remove_user(user_id, password):
     global cursor
     cursor.execute("DELETE FROM users WHERE username = ?", (user_id,))
     conn.commit()
+    
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+            description="Process user intention (add or remove new user")
+            
+    parser.add_argument("action", choiches = ['add','remove'],
+                        help="Add or remove user")
+
+    parser.add_argument('-u', help="user id", required=True, default=None)
+    parser.add_argument('-p', help="user password",
+                        required=False, default=None)
+    args = parser.parse_args()
+    return args
+
+
