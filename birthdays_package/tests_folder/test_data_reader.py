2b1fada6f594c966ea3065cd64a3f12b6fb42614
GianlucaBasso <gb.basso14@gmail.com>
2020-01-09 12:07:34 +0100

    Add valid_file test

--- birthdays_package/tests_folder/test_data_reader.py
+++ birthdays_package/tests_folder/test_data_reader.py
@@ -9,7 +9,18 @@ class Test_data_reader(unittest.TestCase):
         self.tmp_file = '/tmp/temporary_file'
         f = open(self.tmp_file, 'w')
         f.close()
+        # create a valid file
+        self.valid_file = '/tmp/valid_file.csv'
+        f = open(self.valid_file, 'w')
+        f.write('name,birthday\n')
+        f.write('Alan Turing,23/06/1912\n')
+        f.close()
+        
 
+    def test_valid_file(self):
+        people = parse_allowed_people(datafile=self.valid_file)
+        self.assertIsInstance(people, dict)
+        
     def test_empty_datafile(self):
         df = parse_allowed_people(datafile=self.tmp_file)
         self.assertFalse(df)
@@ -24,3 +35,4 @@ class Test_data_reader(unittest.TestCase):
 
     def tearDown(self):
         os.remove(self.tmp_file)
+        os.remove(self.valid_file)
